6767316af4a6da7376591e490d85bb64bb470276
Shaurya Chundawat <chundawatshaurya@gmail.com>
2024-05-23 21:07:28 +0100

    New File

---
+++ TakingInput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Enter a Value: "; // string insertion operator = "<<"
+    int value;
+    cin >> value; // string extraction operator ">>"
+    cout << value;
+    return 0;
+}
